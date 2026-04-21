5a32436f37e8c7b148bcadb270322bcd3675e759
poorva25csu178 <poorva25csu178@ncuindia.edu>
2026-04-21 22:21:23 +0530

    Clean up comments and formatting in count pairs.cpp
    Removed comments and adjusted formatting in count pairs.cpp.

---
+++ count pairs.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        long long total = 1LL * n * (n - 1) / 2;
+
+        map<int, int> mp;
+
+        for (int x : a) {
+            int msb = 31 - __builtin_clz(x); 
+            mp[msb]++;
+        }
+
+        long long bad = 0;
+
+        for (auto it : mp) {
+            long long cnt = it.second;
+            bad += cnt * (cnt - 1) / 2;
+        }
+
+        cout << total - bad << "\n";
+    }
+
+    return 0;
+}
