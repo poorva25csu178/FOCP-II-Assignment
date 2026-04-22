f8721b536c4aee50d4136be31b7ed649cc17115c
poorva25csu178 <poorva25csu178@ncuindia.edu>
2026-04-23 01:14:48 +0530

    Refactor count pairs.cpp for improved readability

--- count pairs.cpp
+++ count pairs.cpp
@@ -1,37 +1,55 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+
+const int MAX = 10000001;
+int spf[MAX];
+
+void sieve() {
+    for (int i = 1; i < MAX; i++) {
+        spf[i] = i;
+    }
+
+    for (int i = 2; i * i < MAX; i++) {
+        if (spf[i] == i) {
+            for (int j = i * i; j < MAX; j += i) {
+                if (spf[j] == j) {
+                    spf[j] = i;
+                }
+            }
+        }
+    }
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int t;
-    cin >> t;
+    sieve();
 
-    while (t--) {
+    int num;
+    cin >> num;
+
+    while (num--) {
         int n;
         cin >> n;
 
-        vector<int> a(n);
-        for (int i = 0; i < n; i++) cin >> a[i];
+        int count = 0;
 
-        long long total = 1LL * n * (n - 1) / 2;
+        while (n > 1) {
+            int p = spf[n];
+            count++;
 
-        map<int, int> mp;
-
-        for (int x : a) {
-            int msb = 31 - __builtin_clz(x); 
-            mp[msb]++;
+            while (n % p == 0) {
+                n = n / p;
+            }
         }
 
-        long long bad = 0;
-
-        for (auto it : mp) {
-            long long cnt = it.second;
-            bad += cnt * (cnt - 1) / 2;
+        int ans = 1;
+        for (int i = 0; i < count; i++) {
+            ans = ans * 2;
         }
 
-        cout << total - bad << "\n";
+        cout << ans << "\n";
     }
 
     return 0;
